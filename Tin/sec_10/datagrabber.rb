d8c9f6ddd30b24eb8cb72ac68f2376d124fa4c13
Kelly Hong <kellyhong@telmate.com>
2016-10-20 17:06:36 -0700

    tin hwk

---
+++ Tin/sec_10/datagrabber.rb
@@ -0,0 +1,14 @@
+class DataGrabber
+
+  # returns away of file contents
+  def file_data(file)
+    File.readlines(file)
+  end
+
+  def clean_up_data_array(array)
+    array.map {|x| x.strip}
+  end
+
+
+
+end
---
+++ Tin/sec_10/tinstudent.rb
@@ -0,0 +1,12 @@
+class Tinstudent
+
+
+
+  def avg_test_score
+
+  end
+
+  def full_name
+
+  end
+end
