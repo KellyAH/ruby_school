b6168f13bd54f0c817b0786f5385a932a09fde7b
Kelly Hong <k_ahong@yahoo.com>
2016-11-08 22:17:51 -0800

    finished hwk

--- ccsf/cs132a_ruby/assn_06/files.rb
+++ ccsf/cs132a_ruby/assn_06/files.rb
@@ -1,3 +1,9 @@
+# Name: Kelly Hong
+# Date: Tuesday, November 8, 2016
+# File: files.rb
+# Desc: write a script that prints a directory listing. list the file names recursively; that is to say, show the names of all the files in all of the subdirectories inside the target directory provided by the user (files in a subdirectory should be indented under that directory name.
+# Assigment: https://ccsf.instructure.com/courses/2612/assignments/25216#submit
+
 require 'fileutils'
 
 @path = ARGV[0].to_s      # capture arguments passed to ruby script and turn path array into string
